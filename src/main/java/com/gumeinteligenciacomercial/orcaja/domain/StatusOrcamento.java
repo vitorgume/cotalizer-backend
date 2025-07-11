fa892962a9475a8427f07d2a2238ffde098b354c
vitorgume <vitorvieira@gumeinteligencia.com.br>
2025-07-11 17:24:33 -0300

    Implementando lógica de status no orçamento

--- src/main/java/com/gumeinteligenciacomercial/orcaja/domain/StatusOrcamento.java
+++ src/main/java/com/gumeinteligenciacomercial/orcaja/domain/StatusOrcamento.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum StatusOrcamento {
     APROVADO(0, "Aprovado"),
-    REPROVADO(1, "Reprovado");
+    REPROVADO(1, "Reprovado"),
+    PENDENTE(2, "Pendente");
 
     private final int codigo;
     private final String descricao;
